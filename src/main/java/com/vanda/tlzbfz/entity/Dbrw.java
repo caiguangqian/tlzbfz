aa6b58d452d961ef60cfc4b3a40a48439f6c8ff4
admin <admin>
2020-12-11 15:13:28 +0800

    提交1

---
+++ src/main/java/com/vanda/tlzbfz/entity/Dbrw.java
@@ -0,0 +1,16 @@
+package com.vanda.tlzbfz.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Date;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dbrw extends VDbrw {
+
+    //发布日期
+    private Date fbrq;
+}
