6273c64b0e12e1d8c804265f121450bf6e54b597
onion <gonion@foxmail.com>
2021-01-04 11:04:30 +0800

    lalala

---
+++ src/main/java/com/vanda/tlzbfz/entity/TUserGw.java
@@ -0,0 +1,18 @@
+package com.vanda.tlzbfz.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName TUserGw
+ * @Description: TODO
+ * @Author onion
+ * @Date 2020/12/28
+ * @Version V1.0
+ **/
+@Data
+public class TUserGw {
+    private String id;
+    private String pk_no;
+    private String gwdm;
+}
+
---
+++ src/main/java/com/vanda/tlzbfz/mapper/TUserGwMapper.java
@@ -0,0 +1,9 @@
+package com.vanda.tlzbfz.mapper;
+
+import com.vanda.tlzbfz.common.config.TkMapper;
+import com.vanda.tlzbfz.entity.TUserGw;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TUserGwMapper extends TkMapper<TUserGw> {
+}
